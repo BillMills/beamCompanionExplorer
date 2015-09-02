d9b7219b21f007c30dbad79efcd780e6d641f716
BillMills <mills.wj@gmail.com>
2015-09-02 04:55:03 -0700

    remember to add new changes to old files :/

--- app/router.js
+++ app/router.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: '/'
 });
 
 Router.map(function() {
+    this.route('companion', { path: 'companion/:species/:A' });
+});
+
+Router.reopen({
+  location: 'hash'
 });
 
 export default Router;
