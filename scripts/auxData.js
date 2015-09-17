afe89507381a533729af573df4d8fa4bee66ae15
BillMills <mills.wj@gmail.com>
2015-09-17 10:49:39 -0700

    laying out first cut page

---
+++ scripts/auxData.js
@@ -0,0 +1,16 @@
+function ulAuxilaryData(route, data){
+
+	var url, path, i;
+
+	//extract the base URL where this page is found.
+	url = window.location.protocol + "//" + window.location.host 
+	path = window.location.pathname.split('/').slice(0,-1);
+	for(i=0; i<path.length; i++){
+		url += path[i] + '/'
+	}
+
+	if(route == "{{species}}/{{A}}"){
+		return {'chargeStates': [1,2,3]}
+	}
+	return {}
+}
